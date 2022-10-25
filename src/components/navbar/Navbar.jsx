df56ede051ba0c89cb93912143be4a485cf01af6
FaisalSaeed-del <shoaibchudhary678@gmail.com>
2022-10-25 12:46:24 +0500

    message

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./navbar.css";
 import Logo from "../../assets/images/logo.svg";
 import Cart from "../../assets/images/cart.svg";
-import {AiOutlineSearch} from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 const Navbar = () => {
   return (
     <div className="bg-image">
       <section className="container">
         <div className="main-nav">
           <div className="logo">
-            <img src={Logo} alt="Logo"  />
+            <img src={Logo} alt="Logo" />
             <span className="text">Daunku</span>
           </div>
           <div className="nav-items">
-            <ul className="nav-menu">
+            <ul>
               <li>
                 {" "}
                 <a href="">Home</a>
@@ -33,27 +33,31 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="shopping-cart">
-            <img src={Cart} alt="shopping cart"/>
+            <img src={Cart} alt="shopping cart" />
           </div>
         </div>
       </section>
       {/* Hero Section */}
       <div className="Hero-section">
         <div className="Hero-Heading">
-          <h2>Bring Serenity to Your Place <br/> With Interior Plants</h2>
+          <h2>
+            Bring Serenity to Your Place <br /> With Interior Plants
+          </h2>
         </div>
         <div className="Hero-para">
-          <p>find your dream plant for you home decoration <br/> with us, and we will make it happen.</p>
+          <p>
+            find your dream plant for you home decoration <br /> with us, and we
+            will make it happen.
+          </p>
         </div>
       </div>
 
       <div className="search-box">
-        <input type="search" name="search" placeholder="Search"/>
+        <input type="search" name="search" placeholder="Search" />
         <div className="icons">
-        <AiOutlineSearch/>
+          <AiOutlineSearch />
         </div>
       </div>
-
     </div>
   );
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./services.css";
 import SHIP from "../../assets/images/shipping.svg";
 import PAY from "../../assets/images/payment.svg";
@@ -7,40 +7,33 @@ import Call from "../../assets/images/call.svg";
 const Services = () => {
   return (
     <div>
-        <section className='container'> 
-            <div className='main-services'>
-                <div className='free-shipping'>
-                    <img src={SHIP} alt='free-shipping'/>
-                    <div className='shipping-text'>
-                        <h3>Free Shipping</h3>
-                        <span>No charge for each delivery</span>
-
-                    </div>
-
-                </div>
-                <div className='free-shipping'>
-                    <img src={PAY} alt='free-shipping'/>
-                    <div className='shipping-text'>
-                        <h3>Quick Payment</h3>
-                        <span>100% secure Payment</span>
-
-                    </div>
-
-                </div>
-                <div className='free-shipping'>
-                    <img src={Call} alt='free-shipping'/>
-                    <div className='shipping-text'>
-                        <h3>24/7 Support</h3>
-                        <span>Quik Support</span>
-
-                    </div>
-
-                </div>
-
+      <section className="container">
+        <div className="main-services">
+          <div className="free-shipping">
+            <img src={SHIP} alt="free-shipping" />
+            <div className="shipping-text">
+              <h3>Free Shipping</h3>
+              <span>No charge for each delivery</span>
             </div>
-        </section>
+          </div>
+          <div className="free-shipping">
+            <img src={PAY} alt="free-shipping" />
+            <div className="shipping-text">
+              <h3>Quick Payment</h3>
+              <span>100% secure Payment</span>
+            </div>
+          </div>
+          <div className="free-shipping">
+            <img src={Call} alt="free-shipping" />
+            <div className="shipping-text">
+              <h3>24/7 Support</h3>
+              <span>Quik Support</span>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
-export default Services
+export default Services;
